9adbfbeca9ff8788907c246cc6cf74f3e02dd2ae
Stefan Emanuelsson <semanuelsson@gmail.com>
2017-11-08 21:35:43 +0100

    Episode 4

--- redo.hs
+++ redo.hs
@@ -1,5 +1,7 @@
-import System.Directory (renameFile, removeFile)
+import Control.Monad (filterM)
+import System.Directory (doesFileExist, renameFile, removeFile)
 import System.Environment (getArgs)
+import System.FilePath (hasExtension, replaceBaseName)
 import System.Exit (ExitCode(..))
 import System.IO (hPutStrLn, stderr)
 import System.Process (createProcess, waitForProcess, shell)
@@ -12,9 +14,22 @@ main = do
 redo :: String -> IO ()
 redo target = do
     let tmp = target ++ "---redoing"
-    (_, _, _, ph) <- createProcess $ shell $ "sh " ++ target ++ ".do - - " ++ tmp ++ " > " ++ tmp
-    exit <- waitForProcess ph
-    case exit of
-        ExitSuccess -> do renameFile tmp target
-        ExitFailure code -> do hPutStrLn stderr $ "Redo script exited with non-zero exit code:" ++ show code
-                               removeFile tmp
+    path <- redoPath target
+    case path of
+        Nothing -> error $ "No .do file found for target '" ++ target ++ "'"
+        Just path -> do
+            (_, _, _, ph) <- createProcess $ shell $ "sh " ++ path ++ " - - " ++ tmp ++ " > " ++ tmp
+            exit <- waitForProcess ph
+            case exit of
+                ExitSuccess -> do renameFile tmp target
+                ExitFailure code -> do hPutStrLn stderr $ "Redo script exited with non-zero exit code:" ++ show code
+                                       removeFile tmp
+
+redoPath :: FilePath -> IO (Maybe FilePath)
+redoPath target = do
+    existingCandidates <- filterM doesFileExist candidates
+    return $ safeHead existingCandidates
+    where candidates = [target ++ ".do"] ++ if hasExtension target then [replaceBaseName target "default" ++ ".do"] else []
+
+safeHead [] = Nothing
+safeHead (x:_) = Just x
